70086c21077e5dea487f126b97f2987620b6f6c9
Abdul Kader <abdulkader0126@gmail.com>
2024-07-17 22:09:22 +0700

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/student';
 
     /**
      * Create a new controller instance.

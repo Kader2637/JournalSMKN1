e71b1b4ae654f8c71ccad1feaf03b381c8e2c240
Abdul Kader <abdulkader0126@gmail.com>
2024-07-17 16:33:56 +0700

    multi role and login register and forgot password

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin', function () {
+    return view('admin.index');
+})->name('admin.index');
+Route::get('/student', function () {
+    return view('student.index');
+})->name('student.index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
